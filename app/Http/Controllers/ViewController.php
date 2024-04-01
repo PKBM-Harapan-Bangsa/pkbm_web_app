8c358ca8b59751f501ad68f4a3e305171f12366a
Ilham Sidik <ilhamsidikc@gmail.com>
2024-04-01 15:30:22 +0700

    meilisearch

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 use Artesaos\SEOTools\Facades\SEOTools;
+use Laravel\Scout\Searchable;
 
 use Illuminate\Http\Request;
 
 class ViewController extends Controller
 {
+    use Searchable;
     public function index(){
         SEOTools::setTitle('PKBM Harapan Bangsa');
-        SEOTools::setDescription('PKBM Harapan Bangsa, Pusat Kegiatan Belajar Masyarakat Kabupaten Banyumas, Kejar Paket A, Setara SD, Kejar Paket B, Setara SMP, Kejar Paket C, Setara SMA');
+        SEOTools::setDescription('PKBM Harapan Bangsa, Pusat Kegiatan Belajar Masyarakat Kabupaten Banyumas');
         return view('welcome');
     }
 }
